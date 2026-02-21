6b7cf01c54b11447ee63600c25398a712e14f789
terraincognita07 <259721513+terraincognita07@users.noreply.github.com>
2026-02-21 21:44:30 +0300

    refactor settings helpers: split cycle helpers

--- internal/api/handlers_settings_profile_helpers.go
+++ internal/api/handlers_settings_profile_helpers.go
@@ -5,7 +5,6 @@ import (
 	"unicode/utf8"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/terraincognita07/lume/internal/models"
 )
 
 const maxDisplayNameLength = 64
@@ -16,34 +15,6 @@ func normalizeDisplayName(raw string) (string, error) {
 		return "", fiber.NewError(fiber.StatusBadRequest, "display name too long")
 	}
 	return displayName, nil
-}
-
-func parseCycleSettingsInput(c *fiber.Ctx) (cycleSettingsInput, string) {
-	input := cycleSettingsInput{}
-	if err := c.BodyParser(&input); err != nil {
-		return cycleSettingsInput{}, "invalid settings input"
-	}
-	if !isValidOnboardingCycleLength(input.CycleLength) {
-		return cycleSettingsInput{}, "cycle length must be between 15 and 90"
-	}
-	if !isValidOnboardingPeriodLength(input.PeriodLength) {
-		return cycleSettingsInput{}, "period length must be between 1 and 10"
-	}
-	return input, ""
-}
-
-func (handler *Handler) saveCycleSettings(userID uint, input cycleSettingsInput) error {
-	return handler.db.Model(&models.User{}).Where("id = ?", userID).Updates(map[string]any{
-		"cycle_length":     input.CycleLength,
-		"period_length":    input.PeriodLength,
-		"auto_period_fill": input.AutoPeriodFill,
-	}).Error
-}
-
-func applyCycleSettings(user *models.User, input cycleSettingsInput) {
-	user.CycleLength = input.CycleLength
-	user.PeriodLength = input.PeriodLength
-	user.AutoPeriodFill = input.AutoPeriodFill
 }
 
 func profileUpdateStatus(previousDisplayName string, updatedDisplayName string) string {
